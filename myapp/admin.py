635ae0c9f92a9a0d7748debc6b276e964a1cefdc
giselle93 <gisellebeatrizbarbosapereira@gmail.com>
2024-12-09 10:53:44 -0300

    Add files via upload
    Adicionando o projeto Django CRUD

---
+++ myapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Tarefa
+
+admin.site.register(Tarefa)
+
+# Register your models here.
---
+++ myapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    criado_em = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titulo
+
+# Create your models here.
